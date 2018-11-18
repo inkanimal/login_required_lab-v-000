33ba45298aaee15d9f20799ac8fd0697c1d4a6d5
geoffrey woodcock <theatomproject@gmail.com>
2018-11-18 21:19:47 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,5 +2,12 @@ class SecretsController < ApplicationController
   
   def show 
     
+     private
+  def require_login
+    unless session.include? :name
+      redirect_to :login
+    end
+  
+    
   end
 end
