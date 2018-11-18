40e65f2fe78123c322ed682df2180a04ec05b5dd
geoffrey woodcock <theatomproject@gmail.com>
2018-11-18 21:22:15 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
    def create
     if params[:name].present?
       session[:name] = params[:name]
-      redirect_to :root
+      redirect_to '/'
     else 
       
       redirect_to :login
